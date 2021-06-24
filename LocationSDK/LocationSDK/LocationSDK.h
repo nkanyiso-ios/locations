425dd02741eb71759bdc1857ceec5e2ab0602800
Nkanyiso <13.nkanyiso@gmail.com>
2021-06-24 22:46:36 +0200

    created location framework

---
+++ LocationSDK/LocationSDK/LocationSDK.h
@@ -0,0 +1,18 @@
+//
+//  LocationSDK.h
+//  LocationSDK
+//
+//  Created by Nkanyiso Hlela on 2021/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocationSDK.
+FOUNDATION_EXPORT double LocationSDKVersionNumber;
+
+//! Project version string for LocationSDK.
+FOUNDATION_EXPORT const unsigned char LocationSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationSDK/PublicHeader.h>
+
+
